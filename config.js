2f6705230481ecf291d18cb77d4c62fcf4134b3f
Teddy Otieno <asolatotieno@gmail.com>
2019-03-21 16:16:26 +0300

    Deletion

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   menu: [
     {
       label: 'Articles',
